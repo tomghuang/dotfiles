7cb10dadd3add0bcdbc5ca4188ad478e3075f227
Tom Huang <tomghuang@gmail.com>
2025-01-11 21:52:06 -0800

    feat: add clink and oh-my-posh config
    In addition to adding config for clink and oh-my-posh, we also enable
    the toggleterm plugin and fix the mode switching issue. Besides, we also
    set the background color of toggleterm terminal.
    
    # [footer]
    
    # //////////////////////////////////////////////////////////////////////////////
    # [title.type]
    #  Must be one of the following:
    #  - fix: Commits that fix bugs
    #  - feat: Commits that add new features
    #  - refactor: Commits that change existing code without changing any behavior
    #  - perf: Special refactor commits that improve performance
    #  - style: Commits that do not affect meaning (white-space, formatting, etc.)
    #  - test: Commits that add tests or fixing existing tests
    #  - docs: Commits that affect documentation only
    #  - build: Commits that change build components (scripts, CI pipeline, etc.)
    #  - chore: Miscellaneous commits (modifying .gitignore, etc.)
    #  - tag: Commits for adding tags
    #
    # [title.scope]
    #  The scope should be the name of the npm package affected. The following is
    #  the list of supported scopes:
    #  - xa: XAgent (default)
    #  - usi: Unified Storage Interface
    #  - uls: Unified Log System
    #
    # [title.summary]
    #  Use the summary field to provide a succinct description of the change:
    #  - Use the imperative, present tense: "change" not "changed" nor "changes"
    #  - Don't capitalize the first letter
    #  - No period (.) at the end
    #
    # [body]
    #  Just as in the summary, use the imperative, present tense: "fix" not "fixed"
    #  nor "fixes".
    #
    #  Explain the motivation for the change in the commit message body. This commit
    #  message should explain why you are making the change. You can include a
    #  comparison of the previous behavior with the new behavior in order to
    #  illustrate the impact of the change.
    #
    # [footer]
    #  The footer can contain information about breaking changes and deprecations
    #  and is also the place to reference GitHub issues, JIRA tickets, and other PRs
    #  that this commit closes or is related to. For example:
    #
    #  Resolves XCAGENT-100
    #  Resolves XCAGENT-100, SEAL-1500, XCAGENT-512
    #
    #  BREAKING CHANGE: <breaking change summary>
    #  <BLANK LINE>
    #  <breaking change description + migration instructions>
    #  <BLANK LINE>
    #  <BLANK LINE>
    #  Resolves <JIRA ticket>
    #
    #  DEPRECATED: <what is deprecated>
    #  <BLANK LINE>
    #  <deprecation description + recommended update path>
    #  <BLANK LINE>
    #  <BLANK LINE>
    #  Resolves #<JIRA ticket>

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # dotfiles
 Configuration for core utilities
+
+## Neovim/LazyVim
+
+Neovim is installed under:
+C:\Opt\nvim-win64
+
+Neovim/LazyVim configuration is under:
+C:\Users\tomgh\AppData\Local\nvim
+
+## Oh My Posh
+
+Oh My Posh is installed in:
+C:\Opt\oh-my-posh
+
+Oh My Posh configuration is in:
+C:\Opt\oh-my-posh\themes
+
+## Clink
+
+Clink is installed under:
+C:\Opt\clink
+
+Clink configuration is under:
+C:\Users\tomgh\clink_settings
+
+## Git
+
+Git configuration is under:
+C:\Users\tomgh\.git-commit-template
+C:\Users\tomgh\.gitconfig
+
--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -1,7 +1,7 @@
 return {
   {
     "akinsho/toggleterm.nvim",
-    enabled = false,
+    enabled = true,
     lazy = true,
     cmd = { "ToggleTerm" },
     keys = {
@@ -69,9 +69,28 @@ return {
       -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
       -- on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
       -- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
+      on_open = function(term)
+        vim.api.nvim_buf_set_keymap(
+          term.bufnr,
+          "t",
+          "<Esc>",
+          "<C-\\><C-n>",
+          { noremap = true, silent = true }
+        )
+      end,
+      highlights = {
+        -- highlights which map to a highlight group name and a table of it's values
+        -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+        Normal = {
+          guibg = "#1f1f1f",
+        },
+        NormalFloat = {
+          link = '#1f1f1f'
+        },
+      },
       hide_numbers = true, -- hide the number column in toggleterm buffers
       shade_filetypes = {},
-      shade_terminals = true,
+      shade_terminals = false,
       -- shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
       start_in_insert = true,
       insert_mappings = true, -- whether or not the open mapping applies in insert mode
@@ -81,6 +100,8 @@ return {
       -- direction = "vertical",
       close_on_exit = true, -- close the terminal window when the process exits
       -- shell = vim.o.shell, -- change the default shell
+      -- shell = "C:/Windows/System32/cmd.exe /s /k C:/Opt/clink/clink_x64.exe inject --profile C:/Users/tomgh",
+      shell = "C:\\Windows\\System32\\cmd.exe /s /k C:\\Opt\\clink\\clink_x64.exe inject",
       -- This field is only relevant if direction is set to 'float'
       -- float_opts = {
       --   -- The border key is *almost* the same as 'nvim_open_win'
